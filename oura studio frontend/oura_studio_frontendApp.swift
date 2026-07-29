8d86b9f8f101d5f7641f37dfaca0fa008b96457e
Handika <handika@atmatech.net>
2026-07-29 17:02:21 +0700

    Initial Commit

---
+++ oura studio frontend/oura_studio_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  oura_studio_frontendApp.swift
+//  oura studio frontend
+//
+//  Created by handika on 29/07/26.
+//
+
+import SwiftUI
+
+@main
+struct oura_studio_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ oura studio frontendTests/oura_studio_frontendTests.swift
@@ -0,0 +1,19 @@
+//
+//  oura_studio_frontendTests.swift
+//  oura studio frontendTests
+//
+//  Created by handika on 29/07/26.
+//
+
+import Testing
+@testable import oura_studio_frontend
+
+struct oura_studio_frontendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
